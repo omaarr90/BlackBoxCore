3ade31294afc41375a7875e207d4bdee6c1f94ee
Omar Alshammari <omar.alshammary@hotmail.com>
2021-02-17 02:05:28 +0300

    Add file encryption

--- Sources/BlackBoxCore/DataEncryptor.swift
+++ Sources/BlackBoxCore/DataEncryptor.swift
@@ -9,22 +9,26 @@ import Foundation
 import CryptoKit
 
 public protocol DataEncryptor {
-    func encrypt(_ plain: [UInt8], key: SymmetricKey) throws -> AES.GCM.SealedBox
+    func encrypt(_ plain: [UInt8],
+                 key: SymmetricKey) throws -> AES.GCM.SealedBox
 }
 
 public protocol DataDecryptor {
-    func decrypt(_ sealed: AES.GCM.SealedBox, key: SymmetricKey) throws -> Data
+    func decrypt(_ sealed: AES.GCM.SealedBox,
+                 key: SymmetricKey) throws -> Data
 }
 
 public struct BlackBoxDataEncryptor: DataEncryptor {
-    public func encrypt(_ plain: [UInt8], key: SymmetricKey) throws -> AES.GCM.SealedBox {
+    public func encrypt(_ plain: [UInt8],
+                        key: SymmetricKey) throws -> AES.GCM.SealedBox {
         let encrypted = try AES.GCM.seal(plain, using: key)
         return encrypted
     }
 }
 
 public struct BlackBoxDataDecryptor: DataDecryptor {
-    public func decrypt(_ sealed: AES.GCM.SealedBox, key: SymmetricKey) throws -> Data {
+    public func decrypt(_ sealed: AES.GCM.SealedBox,
+                        key: SymmetricKey) throws -> Data {
         let decrypted = try AES.GCM.open(sealed, using: key)
         return decrypted
     }
---
+++ Sources/BlackBoxCore/FileEncryptor.swift
@@ -0,0 +1,62 @@
+//
+//  FileEncryptor.swift
+//  
+//
+//  Created by Omar Alshammari on 04/07/1442 AH.
+//
+
+import Foundation
+import CryptoKit
+
+public enum FileEncryptorError: Error {
+    case notFile
+    case noContents
+}
+
+public protocol FileEncryptor {
+    func encrypt(_ url: URL,
+                 to toURL: URL,
+                 key: SymmetricKey,
+                 options: Data.WritingOptions) throws
+}
+
+public protocol FileDecryptor {
+    func decrypt(_ url: URL,
+                 to toURL: URL,
+                 key: SymmetricKey,
+                 options: Data.WritingOptions) throws
+}
+
+public struct BlackBoxFileEncryptor: FileEncryptor {
+    public func encrypt(_ url: URL,
+                        to toURL: URL,
+                        key: SymmetricKey,
+                        options: Data.WritingOptions = .completeFileProtectionUntilFirstUserAuthentication) throws {
+        guard url.isFileURL, toURL.isFileURL else {
+            throw FileEncryptorError.notFile
+        }
+        guard let contents = FileManager.default.contents(atPath: url.path) else {
+            throw FileEncryptorError.noContents
+        }
+        let encrypted = try BlackBoxDataEncryptor().encrypt([UInt8](contents), key: key)
+        try encrypted.combined?.write(to: toURL, options: options)
+    }
+}
+
+public struct BlackBoxFileDecryptor: FileDecryptor {
+    public func decrypt(_ url: URL,
+                        to toURL: URL,
+                        key: SymmetricKey,
+                        options: Data.WritingOptions = .completeFileProtectionUntilFirstUserAuthentication) throws {
+        guard url.isFileURL, toURL.isFileURL else {
+            throw FileEncryptorError.notFile
+        }
+        guard let contents = FileManager.default.contents(atPath: url.path) else {
+            throw FileEncryptorError.noContents
+        }
+        let sealed = try AES.GCM.SealedBox(combined: contents)
+        let open = try BlackBoxDataDecryptor().decrypt(sealed, key: key)
+        try open.write(to: toURL, options: options)
+        fatalError()
+    }
+}
